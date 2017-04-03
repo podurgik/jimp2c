3f38936eac192b7083fa953c6a4e2ed8e9fdd5a8
podurgik <krzysztof.podurgiel.S@ee.pw.edu.pl>
2017-04-03 23:57:44 +0200

    Dodanie funkcji wstrzymującej i dodane czyszczenie ekranu

--- main.c
+++ main.c
@@ -4,17 +4,32 @@
 #include "matrix.h"
 #include "gen.h"
 
+#ifdef _WIN32
+	#include <windows.h>
+	void w(unsigned millis) {
+		Sleep(millis);
+	}
+#else
+	#include <unistd.h>
+	void w(unsigned millis) {
+		sleep(millis / 1000);
+	}
+#endif
+
 int main(int argc, char **argv) {
         int i;
 	int l=atoi(argv[1]);
 	matrix_t * m = wczytaj("matryca.txt");
         matrix_t * m2 = malloc(sizeof(matrix_t));
         m2->tab = (int*) malloc(sizeof(int)* m->n); 
+	
 	for(i=0;i<l;i++) {	
+		printf("\033[H\033[J");
 		gen(m, m2);
 		drukuj(m);
 		move(m, m2);
 		printf("\n");
+		w(1000);
 	}
         //printf("\n\nKolumn: %i\nWierszy:%i\nZnakow: %i\n", m->cols, m->rows, m->n);
 
